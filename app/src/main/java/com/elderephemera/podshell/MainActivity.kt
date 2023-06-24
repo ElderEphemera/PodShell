d27eaa6c1af4128868d74f4340872e2661350a26
Josh Price <2855417+ElderEphemera@users.noreply.github.com>
2023-06-24 13:54:36 -0400

    Start work on UI

--- app/src/main/java/com/elderephemera/podshell/MainActivity.kt
+++ app/src/main/java/com/elderephemera/podshell/MainActivity.kt
@@ -3,16 +3,24 @@ package com.elderephemera.podshell
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.elderephemera.podshell.ui.theme.PodShellTheme
 import kotlinx.coroutines.launch
 
@@ -38,7 +46,9 @@ class MainActivity : ComponentActivity() {
                             }
                         },
                     ) { padding ->
-                        Box(modifier = Modifier.fillMaxSize().padding(padding)) {
+                        Box(modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding)) {
                             Pages(pagerState)
                         }
                     }
@@ -72,10 +82,64 @@ fun Pages(pagerState: PagerState) {
         state = pagerState,
         modifier = Modifier.fillMaxSize(),
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        var showDialog by remember { mutableStateOf(false) }
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { showDialog = true },
+                    backgroundColor = MaterialTheme.colors.primary,
+                ) {
+                    Icon(Icons.Outlined.Add, contentDescription = "Add podcast feed")
+                }
+            },
+        ) { padding ->
+            SubscribeDialog(showDialog = showDialog, setShowDialog = { showDialog = it })
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+
+            }
         }
     }
 }
+
+@Composable
+fun SubscribeDialog(
+    showDialog : Boolean, setShowDialog : (Boolean) -> Unit,
+) =
+    AnimatedVisibility(visible = showDialog) {
+        Dialog(
+            onDismissRequest = { setShowDialog(false) },
+        ) {
+            Column(
+                modifier = Modifier
+                    .background(MaterialTheme.colors.background)
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Text(text = "Add Subscription", fontSize = 20.sp)
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = { Text(text = "Paste feed URL here") },
+                )
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.CenterEnd,
+                ) {
+                    TextButton(
+                        onClick = {},
+                    ) {
+                        Text(text = "SUBSCRIBE")
+                    }
+                }
+            }
+        }
+    }
+
 
 @Preview(showBackground = true)
 @Composable
