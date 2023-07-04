b762b8c3cc2fcab6b8b22ae431036fb521a3cf31
Josh Price <2855417+ElderEphemera@users.noreply.github.com>
2023-07-04 11:05:41 -0400

    Make the feedId column of the episodes table a foreign key

--- app/src/main/java/com/elderephemera/podshell/data/Episode.kt
+++ app/src/main/java/com/elderephemera/podshell/data/Episode.kt
@@ -1,9 +1,20 @@
 package com.elderephemera.podshell.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "episodes")
+@Entity(
+    tableName = "episodes",
+    foreignKeys = [
+        ForeignKey(
+            entity = Feed::class,
+            parentColumns = ["id"],
+            childColumns = ["feedId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+)
 data class Episode(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
