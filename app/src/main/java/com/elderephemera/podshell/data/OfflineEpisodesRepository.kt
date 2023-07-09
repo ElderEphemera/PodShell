32ec08c9823aff77aac888ddf5c451779a7d7813
Josh Price <2855417+ElderEphemera@users.noreply.github.com>
2023-07-08 23:15:05 -0400

    Remove episodes from cache when they are removed from the playlist

--- app/src/main/java/com/elderephemera/podshell/data/OfflineEpisodesRepository.kt
+++ app/src/main/java/com/elderephemera/podshell/data/OfflineEpisodesRepository.kt
@@ -1,8 +1,22 @@
 package com.elderephemera.podshell.data
 
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
+import com.elderephemera.podshell.DownloadsSingleton
+
 class OfflineEpisodesRepository(private val episodeDao: EpisodeDao) : EpisodesRepository {
     override suspend fun insertEpisode(episode: Episode) = episodeDao.insert(episode)
-    override suspend fun updateEpisode(episode: Episode) = episodeDao.update(episode)
+
+    @OptIn(UnstableApi::class)
+    override suspend fun updateEpisode(episode: Episode) {
+        episodeDao.update(episode)
+        if (!episode.inPlaylist) {
+            DownloadsSingleton.getInstance().cacheDataSourceFactory.cache
+                ?.removeResource(episode.guid)
+        }
+    }
+
     override fun getAllFeedEpisodes(feed: Feed) = episodeDao.getAllFromFeed(feed.id)
+
     override fun getAllEpisodesInPlaylist() = episodeDao.getAllInPlaylist()
 }
